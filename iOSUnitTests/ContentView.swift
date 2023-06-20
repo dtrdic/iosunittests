a2cffbadb3bf2a66a61909ef87c54e7412a87b54
Himesh Panchal <106069365+himesh-cm@users.noreply.github.com>
2023-06-20 17:20:01 +0530

    Initial Commit

---
+++ iOSUnitTests/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOSUnitTests
+//
+//  Created by Himesh Panchal on 20/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSUnitTests/iOSUnitTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSUnitTestsApp.swift
+//  iOSUnitTests
+//
+//  Created by Himesh Panchal on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSUnitTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
