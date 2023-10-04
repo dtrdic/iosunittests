32f9dcdc8156a551c25cc663374562bff2096e33
dtrdic <david@nevercode.io>
2023-10-04 11:23:12 +0200

    added one unit test

--- iOSUnitTestsTests/iOSUnitTestsTests.swift
+++ iOSUnitTestsTests/iOSUnitTestsTests.swift
@@ -32,5 +32,16 @@ final class iOSUnitTestsTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+        func testExample() throws {
+        XCTAssertEqual(1 + 1, 2)
+    }
+
+    func testCalculateSum() {
+        let controller = CalculatorController()
+        controller.firstNumber = "2"
+        controller.secondNumber = "3"
+        controller.calculateSum()
+        XCTAssertEqual(controller.result, 5)
+    }
 
 }
