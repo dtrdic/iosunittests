92307694f4ad44d66ee3d423ff9dac36b397db41
dtrdic <david@nevercode.io>
2023-10-04 11:28:26 +0200

    comentout

--- iOSUnitTestsTests/iOSUnitTestsTests.swift
+++ iOSUnitTestsTests/iOSUnitTestsTests.swift
@@ -32,17 +32,17 @@ final class iOSUnitTestsTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
-    func testExample() throws {
-        XCTAssertEqual(1 + 1, 2)
-    }
 
-    func testCalculateSum() {
-        let controller = CalculatorController()
-        controller.firstNumber = "2"
-        controller.secondNumber = "3"
-        controller.calculateSum()
-        XCTAssertEqual(controller.result, 5)
-    }
+    // func testExample() throws {
+    //     XCTAssertEqual(1 + 1, 2)
+    // }
+
+    // func testCalculateSum() {
+    //     let controller = CalculatorController()
+    //     controller.firstNumber = "2"
+    //     controller.secondNumber = "3"
+    //     controller.calculateSum()
+    //     XCTAssertEqual(controller.result, 5)
+    // }
 
 }
